be16cd365597bf00960e70646bbd290f6185b35b
Павел <pavel.tagiltsef@gmail.com>
2021-08-23 18:42:29 +0300

    Создает отображение моковых данных для точки маршрута

--- src/view/event.js
+++ src/view/event.js
@@ -1,31 +1,106 @@
-export const createEventTemplate = () => {
+const chooseRightProposition = (type) => {
+
+  const Preposition = {
+    'taxi': 'to',
+    'bus': 'to',
+    'train': 'to',
+    'ship': 'to',
+    'transport': 'to',
+    'drive': 'to',
+    'flight': 'to',
+    'check-in': 'in',
+    'sightseeing': 'in',
+    'restaurant': 'in',
+  };
+
+  return Preposition[type];
+};
+
+const createSelectedOffers = (offers) => {
+  return offers.map((offer) => {
+    const {title, price} = offer;
+
+    return `<li class="event__offer">
+    <span class="event__offer-title">${title}</span>
+    &plus;
+    &euro;&nbsp;<span class="event__offer-price">${price}</span>
+  </li>`;
+  }).join('');
+};
+
+export const createEventTemplate = (point) => {
+  const {
+    base_price: basePrice,
+    date_from: dateFrom,
+    date_to: dateTo,
+    destination,
+    offers,
+    type,
+  } = point;
+
+  const {
+    name: cityName,
+  } = destination;
+
+  const getDiff = (from, to) => {
+    const diffInMins = to.diff(from, 'minute');
+    const diffInHours = to.diff(from, 'hour');
+    const diffInDays = to.diff(from, 'days');
+
+    const getMins = () => {
+      const mins = diffInMins - diffInHours * 60;
+
+      return mins === 0 ? '' : `${mins}M` ;
+    };
+
+    const getHours = () => {
+      const hours = diffInHours - diffInDays * 24;
+
+      return hours === 0 ? '' : `${hours}H` ;
+    };
+
+    if (diffInHours === 0) {
+      return `${to.diff(from, 'minute')}M`;
+    }
+
+    if (diffInHours >= 1 && diffInHours <= 23) {
+      return `${diffInHours}H ${diffInMins - diffInHours * 60}M`;
+    }
+
+    if (diffInDays >= 1) {
+      return `${diffInDays}D ${getHours()} ${getMins()}`;
+    }
+  };
+
+  const diff = getDiff(dateFrom, dateTo);
+
+  const limitOffers = () => {
+    return offers.length > 3 ? offers.slice(0, 3) : offers;
+  };
+
   return `<li class="trip-events__item">
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi to Amsterdam</h3>
+      <h3 class="event__title">${type} ${chooseRightProposition(type)} ${cityName}</h3>
 
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${dateFrom.format('YYYY-MM-DDTHH:mm:ssZ')}">${dateFrom.format('H:mm')}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${dateTo.format('YYYY-MM-DDTHH:mm:ssZ')}">${dateTo.format('H:mm')}</time>
         </p>
-        <p class="event__duration">30M</p>
+        <p class="event__duration">${diff}</p>
       </div>
 
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
+        &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
       </p>
 
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;
-          &euro;&nbsp;<span class="event__offer-price">20</span>
-        </li>
+        ${createSelectedOffers(limitOffers(offers))}
       </ul>
 
       <button class="event__rollup-btn" type="button">
